1157cf87024067af7c01a7d8d49d21423e80689d
Penguzilla <cmerleigh@gmail.com>
2025-09-10 12:24:01 +0200

    install jsonwebtoken

--- theCaves/index.js
+++ theCaves/index.js
@@ -1,15 +1,15 @@
-
-//cors
 // Importing the required modules
 const express = require("express");
-const cors = require("cors");
+const cors = require("cors"); //cors
+const jwt = require('jsonwebtoken')
+
 // Create an instance of the express server
 const app = express();
 // Define the port number for the server
 const PORT = process.env.PORT || 5000;
 // Enable Cross-Origin Resource Sharing
 app.use(cors());
-// Define the route to retrieve the message
+// Define the route to retrieve the message temp in here
 app.get("/api/data", (req, res) => {
 const data = { message: "Hello from the back end!" };
 res.json(data); // Send data as a response
